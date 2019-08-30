e7ca3cf1322f637c3a31149ae2bed8c1d68d1fd0
Aarjav Chauhan <aarjavchauhan439@gmail.com>
2019-08-29 20:50:54 -0600

    Created 24 spokes and added first Aura light

--- sketch.js
+++ sketch.js
@@ -1,26 +1,29 @@
 let angle = 0
 let spoke_angle = 0
+let wheel_radius = 50
+let wheel_diameter = wheel_radius*2
 
 function setup() {
   createCanvas(500, 500)
-  wheel_radius = 50
-  wheel_diameter = wheel_radius*2
-  inner_wheel_diameter = 80
   angleMode(DEGREES)
 }
 
 function draw() {
-   background(110)
+   background(256)
    draw_front_wheel()
    draw_rear_wheel()
+   spoke_angle++
 }
 
 function draw_front_wheel() {
    push()
    translate(150, 350)
+   //outer wheel
+   circle(0, 0, 110)
+   //inner wheel
    circle(0, 0, wheel_diameter)
    draw_spokes()
-   spoke_angle++
+   rect(50, 5, 5, 5, 20)
    pop()
 }
 
@@ -28,17 +31,19 @@ function draw_spokes()
 {
   rotate(spoke_angle)
   line(0,0, 50, 0)
-  rotate(90)
-  line(0,0, 50, 0)
-  rotate(180)
-  line(0,0, 50, 0)
-  rotate(270)
-  line(0,0, 50, 0)
+  for (var i = 15; i < 360; i+=15) {
+    rotate(15)
+    line(0,0, 50, 0)
+  }
 }
 
 function draw_rear_wheel() {
    push()
    translate(350, 350)
+   //outer wheel
+   circle(0, 0, 110)
+   //inner wheel
    circle(0, 0, wheel_diameter)
+   draw_spokes()
    pop()
 }
